e4411f0c438d7c73da60bf21d2ec9f53a4ee8521
Kunjung <kunjungsherpa@gmail.com>
2025-03-04 18:50:01 +0545

    insert example rows for user table

--- initialize_db_tables.py
+++ initialize_db_tables.py
@@ -4,12 +4,12 @@ if __name__ == '__main__':
     with app.app_context():
         cursor = create_cursor()
 
-        # create user table
+        # Create user table
         cursor.execute('DROP TABLE if exists user')
         cursor.execute('''
                        CREATE TABLE user (
                             id integer primary key auto_increment, 
-                            name varchar(255), 
+                            first_name varchar(255), 
                             last_name varchar(255),
                             email varchar(255),
                             password varchar(500),
@@ -21,11 +21,21 @@ if __name__ == '__main__':
                             updated_at datetime default CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
                             age integer)
                        ''')
-        # cursor.execute("INSERT INTO test_table (name, age) values('gluttony', 25)")
-        # cursor.execute("INSERT INTO test_table (name, age) values('greed', 26)")
+        
+        # load example rows for user table
+        cursor.execute('''
+                       INSERT INTO user (first_name, last_name, email, password, phone, dob, gender, address, created_at, updated_at, age) 
+                            values('Natsuki', 'Subaru', 'subaru@rezero.com', 'subarupassword', '989979966899', '1995-12-26', 'm', 'Tokyo', now(), now(), 29)
+                       ''')
+        cursor.execute('''
+                       INSERT INTO user (first_name, last_name, email, password, phone, dob, gender, address, created_at, updated_at, age) 
+                            values('Emilia', 'Tan', 'emilia@rezero.com', 'emiliapassword', '38375956190', '1997-09-30', 'f', 'Elior Forest', now(), now(), 27)
+                       ''')
         mysql.connection.commit()
         cursor.execute("SELECT * FROM user")    
         test_values = cursor.fetchall()
         # print(type(test_values))
         # print(test_values)
         print(list(test_values))
+
+        # cursor.execute("TRUNCATE TABLE user")
